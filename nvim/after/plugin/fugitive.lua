a27692109c53fe517bafd3248d476cd3a593f1df
lucasdsht <lucasdouchet17112003@gmail.com>
2023-09-25 22:45:40 +0200

    feat: some fugitive stuff

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,5 +1,12 @@
+-- classic stuff
+
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
 vim.keymap.set("n", "<leader>gl", ":Git pull<CR>")
 vim.keymap.set("n", "<leader>gb", ":Git branch")
 vim.keymap.set("n", "<leader>gco", ":Git checkout")
+
+-- rebase stuff
+
+vim.keymap.set("n", "<leader>gj", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "<leader>gk", "<cmd>diffget //3<CR>")
