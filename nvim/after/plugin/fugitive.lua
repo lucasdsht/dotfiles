0f35d41b14c8f2898afb74516c1867aaee7b5cc1
lucasdsht <lucasdouchet17112003@gmail.com>
2023-09-25 22:07:55 +0200

    feat: fugitive workflow

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gp", ":Git push")
+vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
+vim.keymap.set("n", "<leader>gl", ":Git pull<CR>")
+vim.keymap.set("n", "<leader>gb", ":Git branch")
+vim.keymap.set("n", "<leader>gco", ":Git checkout")
