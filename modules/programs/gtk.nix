25da60374f1b6e5562bb486d769f2a8f1768e084
lucasdsht <lucasdouchet17112003@gmail.com>
2026-01-05 14:33:08 +0100

    fix gtk

--- modules/programs/gtk.nix
+++ modules/programs/gtk.nix
@@ -2,19 +2,33 @@
 
 {
   gtk = {
+    enable = true;
+
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+
     iconTheme = {
       name = "Tela-dark"; 
       package = pkgs.tela-icon-theme;
     };
+    
+    cursorTheme = {
+      name = "Graphite-dark";
+      package = pkgs.graphite-cursors;
+      size = 24;
+    };
+
+    
+    gtk3.extraCss = ''
+      @import "colors.css";
+    '';
+
+    gtk4.extraCss = ''
+      @import "colors.css";
+    '';
 
   };
-
-  xdg.configFile."gtk-3.0/gtk.css".text = ''
-    @import url("colors.css");
-  '';
-
-  xdg.configFile."gtk-4.0/gtk.css".text = ''
-    @import url("colors.css");
-  '';
 }
 
