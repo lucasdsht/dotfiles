8d03730c9f01d82dad142e6f973ed5f45b7c104c
lucasdsht <lucasdouchet17112003@gmail.com>
2026-02-11 18:53:42 +0100

    add go develop env

--- shells/golang.nix
+++ shells/golang.nix
@@ -1,5 +1,34 @@
-{ pkgs }:
+{
+  description = "Go development environment";
 
-pkgs.mkShell {
-  name = "golang-env";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        go
+        gopls
+        delve
+        golangci-lint
+        gotools
+        git
+      ];
+
+      shellHook = ''
+        export GOPATH=$PWD/.go
+        export GOMODCACHE=$GOPATH/pkg/mod
+        export PATH=$GOPATH/bin:$PATH
+
+        echo "Go dev environment ready"
+        go version
+      '';
+    };
+  };
 }
+
